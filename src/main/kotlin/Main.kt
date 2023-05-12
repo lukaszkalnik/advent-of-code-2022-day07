c69017c0720644c477d136a1219eb75922b6d5eb
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-05-12 22:21:19 +0200

    Move Path outside of Line hierarchy; print parse output for testing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
 import okio.FileSystem
 import okio.Path.Companion.toPath
 
-sealed interface Line {
+sealed interface Path {
+    object Root : Path
+    object Up : Path
+    data class Directory(val name: String) : Path
+}
 
-    sealed interface Path {
-        object Root : Path
-        object Up : Path
-        data class Directory(val name: String) : Path
-    }
+sealed interface Line {
 
     sealed interface Command {
         data class Cd(val path: Path) : Line
         object Ls : Line
     }
 
-    sealed interface HierarchyItem {
+    sealed interface FileSystemItem {
         data class File(val size: Int, val name: String) : Line
         data class Directory(val name: String)
     }
@@ -23,25 +23,25 @@ sealed interface Line {
 fun main(args: Array<String>) {
     val path = "input.txt".toPath()
     val input = FileSystem.SYSTEM.read(path) { readUtf8() }.dropLast(1)
-
-
+    input.split("\n").map { line -> parse(line) }.forEach { println(it) }
 }
 
 fun parse(line: String): Line =
     when {
         line == "$ ls" -> Line.Command.Ls
         line.startsWith("$ cd") -> parseCd(line)
-        line.startsWith("dir") -> parseDirectory(line)
-        """\d+ .*""".toRegex() matches line -> parseFile(line)
+        line.startsWith("dir") -> Line.Command.Ls//parseDirectory(line)
+        """\d+ .*""".toRegex() matches line -> Line.Command.Ls //parseFile(line)
+        else -> throw IllegalStateException("unrecognized line: $line")
     }
 
 fun parseCd(line: String): Line.Command.Cd {
     val result = """\$ cd (.*)""".toRegex().matchEntire(line)
     val pathString = result?.groups?.get(1)?.value ?: throw IllegalStateException("cd command path is null")
     val path = when (pathString) {
-        "/" -> Line.Path.Root
-        ".." -> Line.Path.Up
-        else -> Line.Path.Directory(pathString)
+        "/" -> Path.Root
+        ".." -> Path.Up
+        else -> Path.Directory(pathString)
     }
     return Line.Command.Cd(path)
 }
